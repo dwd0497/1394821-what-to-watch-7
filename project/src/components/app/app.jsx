cd290c70d4383ab3550a0940f4efdeeedfd91d6e
dwd0497 <dwd0497@gmail.com>
2021-06-05 15:20:45 +0500

    Добавил компоненты главной страницы и карточки фильма

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+function App({promoFilm, filmsCount}) {
+  return (
+    <Main promoFilm={promoFilm} filmsCount={filmsCount} />
+  );
 }
+
+App.propTypes = {
+  filmsCount: PropTypes.number.isRequired,
+  promoFilm: PropTypes.objectOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      genre: PropTypes.string.isRequired,
+      date: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const promoFilm = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  date: '2014',
+};
+
+const FILMS_COUNT = 20;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App promoFilm={promoFilm} filmsCount={FILMS_COUNT} />
   </React.StrictMode>,
   document.getElementById('root'));
